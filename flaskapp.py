8b68aa9df070e1e634867932ad30a1e727c7a52e
Dhiraj Beri <46566839+dhirajberi@users.noreply.github.com>
2021-09-30 13:41:56 +0530

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -20,42 +20,51 @@ def papers(username):
     from webdriver_manager.chrome import ChromeDriverManager
 
     #THIS INITIALIZES THE DRIVER (AKA THE WEB BROWSER)
+    CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
     options = webdriver.ChromeOptions()
     options.headless = True
-    driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
+    # driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
+    driver = webdriver.Chrome("./chromedriver",options=options)
 
     #THIS PRETTY MUCH TELLS THE WEB BROWSER WHICH WEBSITE TO GO TO
     driver.get(f'https://tokcounter.com/?user={username}')
 
     time.sleep(5)
-    FOLLOWERS = driver.find_element_by_xpath('//*[@id="__next"]/div/div/div[4]/div[1]/div').text
-    LIKES = driver.find_element_by_xpath('//*[@id="__next"]/div/div/div[4]/div[2]/div[1]/div/div/div').text
-    FOLLOWING = driver.find_element_by_xpath('//*[@id="__next"]/div/div/div[4]/div[2]/div[2]/div/div/div/span/span[2]/span/span/span').text
-    UPLOADS = driver.find_element_by_xpath('//*[@id="__next"]/div/div/div[4]/div[2]/div[3]/div/div/div').text
+    try:
+        FOLLOWERS = driver.find_element_by_xpath('//*[@id="__next"]/div/div/div[4]/div[1]/div').text
+        LIKES = driver.find_element_by_xpath('//*[@id="__next"]/div/div/div[4]/div[2]/div[1]/div/div/div').text
+        FOLLOWING = driver.find_element_by_xpath('//*[@id="__next"]/div/div/div[4]/div[2]/div[2]/div/div/div/span/span[2]/span/span/span').text
+        UPLOADS = driver.find_element_by_xpath('//*[@id="__next"]/div/div/div[4]/div[2]/div[3]/div/div/div').text
     
-    #PRINTS OUT THE DATA PULLED FROM ABOVE
-    followers=FOLLOWERS.split('\n')
-    followers="".join(followers)
-    
-    likes = LIKES.split('\n')
-    likes="".join(likes)
+        #PRINTS OUT THE DATA PULLED FROM ABOVE
+        followers=FOLLOWERS.split('\n')
+        followers="".join(followers)
+        
+        likes = LIKES.split('\n')
+        likes="".join(likes)
 
-    following = FOLLOWING.split('\n')
-    following="".join(following)
+        following = FOLLOWING.split('\n')
+        following="".join(following)
 
-    uploads = UPLOADS.split('\n')
-    uploads="".join(uploads)
+        uploads = UPLOADS.split('\n')
+        uploads="".join(uploads)
 
-    details = {
-        "Username": username,
-        "Live Followers": followers,
-        "Likes":likes,
-        "Following":following,
-        "Uploads": uploads,
-        # "Followers Rank": followers_rank
-    }
+        details = {
+            "Username": username,
+            "Live Followers": followers,
+            "Likes":likes,
+            "Following":following,
+            "Uploads": uploads,
+            # "Followers Rank": followers_rank
+        }
 
-    return jsonify(details)
+        return jsonify(details)
+    except:
+        details = {
+            "Message":"Something went wrong, please try again!"
+        }
+        return jsonify(details)
 
-if __name__ == '__main__':
-    app.run()
+
+if _name_ == '__main__':
+    app.run(host="0.0.0.0",port=8080)
